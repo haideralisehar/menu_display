c71a19129ed643096ccf86caa8fede7a7ff1abaa
Haider Ali Sehar <haideralisehar254@gmail.com>
2025-10-27 15:45:29 +0500

    Add

---
+++ src/app/menu/page.jsx
@@ -0,0 +1,418 @@
+"use client";
+import "./page.css";
+
+export default function HomePage() {
+  return (
+    <main className="price-container">
+      <div className="content">
+        <h1 className="main-title">Price List</h1>
+        <p className="subtitle">thread & wax</p>
+
+        <div className="price-section">
+          {/* Left Column - Threading */}
+          <div className="column">
+            <h2 className="section-title">Threading</h2>
+            <div className="item">
+              <span>Upper Lip</span>
+              <span>1.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Chin</span>
+              <span>1.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Eyebrows</span>
+              <span>2.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Full Face</span>
+              <span>3.000 BD</span>
+            </div>
+          </div>
+
+          
+
+          {/* Right Column - Waxing */}
+          <div className="column">
+            <h2 className="section-title">Waxing</h2>
+            <div className="item">
+              <span>Upper Lip</span>
+              <span>1.500 BD</span>
+            </div>
+            <div className="item">
+              <span>Chin</span>
+              <span>1.500 BD</span>
+            </div>
+            <div className="item">
+              <span>Eyebrows</span>
+              <span>2.500 BD</span>
+            </div>
+            <div className="item">
+              <span>Full Face</span>
+              <span>3.500 BD</span>
+            </div>
+          </div>
+          {/* Divider Line Nail services */}
+          <div className="divider"></div>
+           <h1 className="main-title">Price List</h1>
+        <p className="subtitle">Nail services</p>
+
+          {/* Right Column - Waxing */}
+          <div className="column">
+            <h2 className="section-title">Nail Services</h2>
+            
+            <div className="item">
+              <span>Cut & File (shape) no water</span>
+              <span>4.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Normal Nail Polish</span>
+              <span>2.000 – 3.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Gel Nail Polish (includes cuticle work)</span>
+              <span>10.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Normal Nail Polish Remover</span>
+              <span>1.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Gel Nail Polish Remover</span>
+              <span>5.000 BD</span>
+            </div>
+            <br />
+
+            <div className="item">
+              <span>Classic Manicure 30min</span>
+              <span>6.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Classic Pedicure 45min</span>
+              <span>8.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Classic mani+ Pedi Package</span>
+              <span>12.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Voesh Manicure 45min</span>
+              <span>8.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Voesh Pedicure 1hour</span>
+              <span>10.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Voesh-Mani+Pedi Package</span>
+              <span>16.000 BD</span>
+            </div>
+            
+            
+
+            <div className="column">
+            <h2 className="section-title">Nail Extensions</h2>
+            
+            <div className="item">
+              <span>Soft Gel Nail Extension 
+2hour (includes normal polish)</span>
+              <span>20.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Soft Gel Nail Extension 
+2h30min Gel Polish</span>
+              <span>25.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Poly gel Nail Extension</span>
+              <span>35.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Poly gel Nail Refill</span>
+              <span>25.000 BD</span>
+            </div>
+            <h2 className="section-title">Nail Art</h2>
+
+
+            <div className="item">
+              <span>Gems and Sticker</span>
+              <span>0.500 BD per nail</span>
+            </div>
+           
+
+            <div className="item">
+              <span>Free hand and 3D</span>
+              <span>1.000 BD per nail</span>
+            </div>
+            <div className="item">
+              <span>Poly gel Nail Extension</span>
+              <span>5.000 BD</span>
+            </div>
+            <div className="item">
+              <span>French</span>
+              <span>5.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Chrome</span>
+              <span>5.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Ombre</span>
+              <span>5.000 BD</span>
+            </div>
+            
+            <div className="divider"></div>
+            
+          </div>
+        
+
+          </div>
+          <h1 className="main-title">Price List</h1>
+        <p className="subtitle">Nail services – Add on’s</p>
+
+         <div className="column">
+            
+            
+            <div className="item">
+              <span>Voesh Callus remover 
+pedicure (add on)</span>
+              <span>3.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Voesh collagene gloves</span>
+              <span>3.500 BD</span>
+            </div>
+            <div className="item">
+              <span>add on massage (10min)</span>
+              <span>5.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Princess Manicure + 
+Pedicure (under 12yr)</span>
+              <span>8.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Spa Mani + Pedi (Voesh is Spa)</span>
+              <span>?.000 BD</span>
+            </div>
+            
+            <h2 className="section-title">Extention/Gel 
+Polish Removal</h2>
+
+            <div className="item">
+              <span>Acetone Soak off (includes mani)</span>
+              <span>5.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Drill machine removal</span>
+              <span>8.000 BD</span>
+            </div>
+            
+            <div className="divider"></div>
+            
+              
+
+
+            
+
+           
+        
+
+          </div>
+           <h1 className="main-title">Price List</h1>
+        <p className="subtitle">Hair services</p>
+
+        <div className="column">
+            <h2 className="section-title">Wash</h2>
+
+            <div className="item">
+              <span>Hair Wash</span>
+              <span>3.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Hair Wash with Mask</span>
+              <span>6.000 BD</span>
+            </div>
+
+             <h2 className="section-title">Haircuts</h2>
+
+            <div className="item">
+              <span>Trim</span>
+              <span>3.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Cut</span>
+              <span>From 5.000 BD</span>
+            </div>
+
+            <h2 className="section-title">Hairstyling</h2>
+
+            <div className="item">
+              <span>Blow Dry</span>
+              <span>From 6.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Wavy</span>
+              <span>7.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Hairstyle</span>
+              <span>10.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Flat Iron</span>
+              <span>5.000 BD</span>
+            </div>
+            
+        </div>
+        <div className="divider"></div>
+        <h1 className="main-title">Price List</h1>
+        <p className="subtitle">Hair Treatments</p>
+
+        <div className="column">
+            <h2 className="section-title">Natural 
+Treatments</h2>
+
+            <div className="item">
+              <span>Aloe Vera</span>
+              <span>8.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Coconut</span>
+              <span>8.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Tea Tree</span>
+              <span>12.000 BD</span>
+            </div>
+            
+
+             <h2 className="section-title">Damaged Hair 
+Treatments</h2>
+
+            <div className="item">
+              <span>K18</span>
+              <span>25.000 - 35.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Nashi-Filier Therapy 3 Steps</span>
+              <span>15.000 – 25.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Nashi-Filier Therapy lifting 
+express</span>
+              <span>10.000 – 18.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Nashi Argan essential 
+energy - Hair Falling</span>
+              <span>10.000BD</span>
+            </div>
+
+            <h2 className="section-title">Brazillian 
+Treatments</h2>
+
+            <div className="item">
+              <span>BK ecosmetics-Keratine 
+Hair-Blue</span>
+              <span>50.000 – 120.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Ecosmetics Plastica Repairs-Pink</span>
+              <span>15.000 – 30.000 BD</span>
+            </div>
+            <div className="item">
+              <span>BC ecosmetics-Antifrizz </span>
+              <span>35.000 – 60.000 BD</span>
+            </div>
+            <div className="item">
+              <span>BK</span>
+              <span>10.000 – 20.000 BD</span>
+            </div>
+             <h2 className="section-title">Nashi Treatments</h2>
+             <div className="item">
+              <span>Blond Joy</span>
+              <span>3.000 – 5.000BD</span>
+            </div>
+            <div className="item">
+              <span>Nashi Argan Deep mask</span>
+              <span>12.000 – 20.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Armonia </span>
+              <span>12.000 – 20.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Mass Solution</span>
+              <span>12.000 – 20.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Henna</span>
+              <span>5.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Sider </span>
+              <span>5.000 BD</span>
+            </div>
+
+
+        </div>
+         <div className="divider"></div>
+        <h1 className="main-title">Price List</h1>
+        <p className="subtitle">Hair Color</p>
+
+        <div className="column">
+           
+
+            <div className="item">
+              <span>Roots</span>
+              <span>20.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Roots Ammonia Free</span>
+              <span>25.000 BD</span>
+            </div>
+            <br />
+            <div className="item">
+              <span>Full Color</span>
+              <span>45.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Full Color Ammonia 
+Free</span>
+              <span>50.000 BD</span>
+            </div>
+            <br /><br />
+
+            <div className="item">
+              <span>Balyage & Hair 
+Lightening</span>
+              <span>From 70.000 BD</span>
+            </div>
+            <div className="item">
+              <span>Toner</span>
+              <span>From 35.000 BD</span>
+            </div>
+            
+            
+
+             
+
+
+        </div>
+
+
+
+
+        </div>
+      </div>
+
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="logo-circle">✨</div>
+          <p>@glowup_salon.bh | +973 37 755 515</p>
+        </div>
+      </footer>
+    </main>
+  );
+}
